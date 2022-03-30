2e895054412eced0c164c1f3a5074f3a32289aa2
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-03-30 11:37:20 +0300

    create delete item handle Relates #7

--- src/components/ListItems/ListItems.jsx
+++ src/components/ListItems/ListItems.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './ListItems.css';
 const Listitem = (props) => {
-  const { items } = props;
-  const ListItems = items.map((item) => {
-    return (
-      <li key={item.id}>
-        <span>{item.desc}</span>
-        <span>
-          <i class="fa-solid fa-highlighter"></i>
-        </span>
-        <span>
-          <i class="fa-solid fa-circle-xmark"></i>
-        </span>
-        <span>
-          <i class="fa-solid fa-pen-to-square"></i>
-        </span>
-      </li>
-    );
-  });
+  const { items, deleteItem } = props;
+  const ListItems = items.length ? (
+    items.map((item) => {
+      return (
+        <li key={item.id}>
+          <span>{item.desc}</span>
+          <span>
+            <i className="fa-solid fa-highlighter"></i>
+          </span>
+          <span onClick={() => deleteItem(item.id)}>
+            <i className="fa-solid fa-circle-xmark"></i>
+          </span>
+          <span>
+            <i className="fa-solid fa-pen-to-square"></i>
+          </span>
+        </li>
+      );
+    })
+  ) : (
+    <p>No ToDo Item</p>
+  );
   return (
     <div>
       <ul>{ListItems}</ul>
