e14a40e51477b395b2b672b18f1dc383253771f1
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-03-30 22:15:31 +0300

    create edit functionailty and mark Relates #15

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Listitem from './components/ListItems/ListItems';
+import ListItem from './components/ListItems/ListItems';
 import './App.css';
-import Additem from './components/Additem/Additem';
+import AddItem from './components/Additem/Additem';
 class App extends React.Component {
   state = {
     items: [
-      { id: 1, desc: 'hello World1' },
-      { id: 2, desc: 'hello World2' },
-      { id: 3, desc: 'hello World3' },
-      { id: 4, desc: 'hello World4' },
+      { id: 1, desc: 'Hello1', Done: false },
+      { id: 15, desc: 'Hello3', Done: false },
     ],
   };
 
@@ -17,21 +15,47 @@ class App extends React.Component {
     this.setState({ items });
   };
 
-  addItem=(item)=>{
-    item.id=Math.random
-    let items=this.state.items
-    items.push(item)
-    this.setState({ items })
+  addItem = (item) => {
+    item.id = Date.now();
+    item.Done = false;
+    let items = this.state.items;
+    items.push(item);
+    this.setState({ items });
+  };
 
-  }
+  editItem = (id, desc) => {
+    const { items } = this.state;
+    let item = items[id];
+    item.desc = desc;
+    this.setState({
+      items,
+    });
+  };
+  markItem = (id) => {
+    this.setState((prevStat) => {
+      prevStat.items[id].Done = true;
+      return prevStat;
+    });
+  };
+  removeMark = (id) => {
+    this.setState((prevStat) => {
+      prevStat.items[id].Done = false;
+      return prevStat;
+    });
+  };
 
   render() {
     const { items } = this.state;
     return (
       <div className="App">
-        <Additem addItem={this.addItem} />
-        <Listitem items={items} deleteItem={this.deleteItemHandle} />
-        
+        <AddItem addItem={this.addItem} />
+        <ListItem
+          items={items}
+          deleteItem={this.deleteItemHandle}
+          editItem={this.editItem}
+          markItem={this.markItem}
+          removeMark={this.removeMark}
+        />
       </div>
     );
   }
